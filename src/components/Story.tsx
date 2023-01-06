362da819872633468ca79f8a033390f885982fe2
Sebascode <nahuelsebastiandiaz@icloud.com>
2023-01-06 17:42:01 -0300

    Test play

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -1,10 +1,14 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { UIContext } from "../context/UI";
 import styles from "../styles/modules/UI/Story.module.scss";
 import Portal from "./Portal";
 
 export const Story = ({ video }: { video: string }) => {
   const { hideComponent } = useContext(UIContext);
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const playHandler = () => videoRef.current && videoRef.current.play();
 
   const closeHandler = () => {
     document.body.style.overflow = "scroll";
@@ -13,14 +17,15 @@ export const Story = ({ video }: { video: string }) => {
 
   return (
     <Portal>
-      <div className={styles.container}>
+      <div onTouchStart={playHandler} className={styles.container}>
         <video
+          ref={videoRef}
           onEnded={closeHandler}
-          src={video}
           autoPlay
-          controls
+          src={video}
           preload="auto"
         />
+        <i onClick={closeHandler} id="close" className="bx bx-x"></i>
       </div>
     </Portal>
   );
