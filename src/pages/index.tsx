dd50dd0fc7b7d980a0b5e514f0606f01261ec7c2
Sebascode <nahuelsebastiandiaz@icloud.com>
2023-01-20 10:03:54 -0300

    Facebook tracking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Reactpixel, { pageView } from "react-facebook-pixel";
 import { HeroSection } from "../components/HeroSection";
 import { Public as Layout } from "../layouts/Public";
 import { Testimonials } from "../components/Testimonials";
@@ -5,6 +6,9 @@ import { Questioner } from "../components/Questioner/Questioner";
 import { HowItWorks } from "../components/HowItWorks";
 
 const index = () => {
+  Reactpixel.init("993435672043956");
+  pageView();
+
   return (
     <Layout>
       <HeroSection />
